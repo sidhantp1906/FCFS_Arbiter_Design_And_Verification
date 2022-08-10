8608a76a01a63daeb6391766c5d03a4aad72b7f4
Sidhant priyadarshi <60102705+sidhantp1906@users.noreply.github.com>
2022-08-10 15:44:36 +0530

    Create Interface.sv

---
+++ Interface.sv
@@ -0,0 +1,14 @@
+interface intf(input logic clk,resetn);
+  
+  //declaring the signals
+  logic Rqst0;
+  logic Rqst1;
+  logic Rqst2;
+  logic Rqst3;
+logic Grant0;
+  logic Grant1;
+  logic Grant2;
+  logic Grant3;
+
+  
+endinterface
